5c8f790755ab4dd0f47d1ba5daf140084d7eb050
Piyush Kumar Pradhan <kumarpradhanpiyush77@gmail.com>
2023-05-30 20:05:14 +0530

    added commentDetails props inside approvalcards prop

--- src/index.js
+++ src/index.js
@@ -2,17 +2,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentDetails from './CommentDetails'
+import ApprovalCard from './ApprovalCard';
 import { faker } from '@faker-js/faker';
 
 // Create a react component
 const App = function () {
     return (
         <div>
+
             <div className='ui container comments'>
-                <CommentDetails author="Sam" timeAgo="6:40 PM" commentSaid="LOL" avatar={faker.image.avatar()} />
-                <CommentDetails author="Alex" timeAgo="5:40 PM" commentSaid="Nice Work" avatar={faker.image.avatar()} />
-                <CommentDetails author="Bem" timeAgo="2:10 AM" commentSaid="Keep it work" avatar={faker.image.avatar()} />
-                <CommentDetails author="James" timeAgo="3:10 AM" commentSaid="I am a comment nothing else" avatar={faker.image.avatar()} />
+                <ApprovalCard>
+                    <CommentDetails author="Sam" timeAgo="6:40 PM" commentSaid="LOL" avatar={faker.image.avatar()} />
+                </ApprovalCard>
+                <ApprovalCard>
+                    <CommentDetails author="Alex" timeAgo="5:40 PM" commentSaid="Nice Work" avatar={faker.image.avatar()} />
+                </ApprovalCard>
+                <ApprovalCard>
+                    <CommentDetails author="Bem" timeAgo="2:10 AM" commentSaid="Keep it work" avatar={faker.image.avatar()} />
+                </ApprovalCard>
+                <ApprovalCard>
+                    <CommentDetails author="James" timeAgo="3:10 AM" commentSaid="I am a comment nothing else" avatar={faker.image.avatar()} />
+                </ApprovalCard>
             </div>
         </div>
     )
